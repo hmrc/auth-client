e1b22aa94f2bf4fde2a1886ae0ffc8947c971177
Steve Edmans <30315073+steve-edmans@users.noreply.github.com>
2021-01-25 10:27:17 +0000

    SIS-1551: Remove play 2.5 version (#47)
    * SIS-1551: Remove Play 2.5 version
    
    * SIS-1551: Update README
    
    * SIS-1551: Remove play 2.5 from travis

--- project/BuildDependencies.scala
+++ project/BuildDependencies.scala
@@ -5,7 +5,6 @@ private object BuildDependencies {
 
   val compile: Seq[ModuleID] = dependencies(
     shared = Seq("com.iheart" %% "ficus" % "1.4.7"),
-    play25 = Seq("uk.gov.hmrc" %% "http-verbs-play-25" % "12.3.0" % Provided),
     play26 = Seq("uk.gov.hmrc" %% "http-verbs-play-26" % "12.3.0" % Provided),
     play27 = Seq("uk.gov.hmrc" %% "http-verbs-play-27" % "12.3.0" % Provided)
   )
@@ -15,7 +14,6 @@ private object BuildDependencies {
                  "org.mockito" % "mockito-core" % "2.10.0" % Test,
                  "org.scalamock" %% "scalamock" % "4.4.0" % Test
     ),
-    play25 = Seq("org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % Test),
     play26 = Seq("org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3" % Test),
     play27 = Seq("org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % Test)
   )
