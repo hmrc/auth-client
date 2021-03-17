40510f930692be52aafe693b36f8ec730f904941
martinarmstrong <martin.armstrong@digital.hmrc.gov.uk>
2021-03-17 11:38:35 +0000

    GG-5484 - add integration test from auth-client-tests

---
+++ src/it/scala/uk/gov/hmrc/core/retrieve/v2/RetrievalsSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.core.retrieve.v2
+
+import uk.gov.hmrc.auth.core.{ConfidenceLevel, MissingBearerToken}
+import uk.gov.hmrc.auth.core.retrieve.Credentials
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.core.utils.{AuthUtils, BaseSpec}
+
+class RetrievalsSpec extends BaseSpec  with AuthUtils {
+
+  "Credentials" should {
+    "retrieve the correct data" in {
+      val credId = randomCredId
+      implicit val hc = signInGGWithAuthLoginApi(credId)
+      val creds: Option[Credentials] = awaitAuth(authorised(ConfidenceLevel.L250).retrieve(Retrievals.credentials))
+      creds shouldBe Some(Credentials(credId, "GovernmentGateway"))
+    }
+    "retrieve CL250 as CL250 i.e. not downgraded to CL200" in {
+      val credId = randomCredId
+      implicit val hc = signInGGWithAuthLoginApi(credId)
+      val cl: ConfidenceLevel = awaitAuth(authorised().retrieve(Retrievals.confidenceLevel))
+      cl shouldBe ConfidenceLevel.L250
+    }
+  }
+
+  "Authorisation" should {
+    "fail" when {
+      "no token is provided" in {
+        implicit val hc = HeaderCarrier()
+        assertThrows[MissingBearerToken] {
+          awaitAuth(authorised())
+        }
+      }
+    }
+  }
+
+
+}
---
+++ src/it/scala/uk/gov/hmrc/core/utils/AuthUtils.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.core.utils
+
+import java.util.UUID
+import play.api.libs.json.{Json, OFormat}
+import uk.gov.hmrc.auth.core.{AuthorisedFunctions, Enrolment, EnrolmentIdentifier}
+import uk.gov.hmrc.http.{Authorization, HeaderCarrier, HeaderNames}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Random
+
+trait AuthUtils extends AuthorisedFunctions {
+
+  this: BaseSpec =>
+
+  val random = new Random
+  def randomNino = s"AA${900000 + random.nextInt(99999)}D"
+  def randomCredId = s"credId-${random.nextInt(10101010)}"
+  def randomGroupId = UUID.randomUUID().toString
+
+  def awaitAuth[T](f: AuthorisedFunctionWithResult[T])(implicit hc: HeaderCarrier): T = await(f(Future.successful))
+  def awaitAuth(f: AuthorisedFunction)(implicit hc: HeaderCarrier): Unit = await(f(Future.successful(())))
+
+  def authLoginApiResource(resource: String): String = s"http://localhost:8585$resource"
+
+  def signInGGWithAuthLoginApi(credId: String = randomCredId, enrolments: Set[Enrolment] = Set.empty, nino: Option[String] = None, groupId: String = randomGroupId): HeaderCarrier = {
+    implicit val idFormat: OFormat[EnrolmentIdentifier] = Json.format[EnrolmentIdentifier]
+    implicit val enrolmentsFormat: OFormat[Enrolment] = Json.format[Enrolment]
+
+    val request = Json.obj(
+      "credId" -> credId,
+      "groupIdentifier" -> groupId,
+      "affinityGroup" -> "Individual",
+      "confidenceLevel" -> 250,
+      "credentialStrength" -> "strong",
+      "enrolments" -> Json.toJson(enrolments)
+    ) ++ nino.map(n => Json.obj("nino" -> nino)).getOrElse(Json.obj())
+    val exchangeResult = withClient { ws => await(ws.url(authLoginApiResource("/government-gateway/session/login")).post(request)) }
+
+    exchangeResult.status shouldBe 201
+
+    HeaderCarrier(
+      authorization = exchangeResult.header(HeaderNames.authorisation) map Authorization
+    )
+
+  }
+
+}
