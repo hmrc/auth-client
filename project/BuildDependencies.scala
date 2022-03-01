97d828e586049b05c69c0cadb9708b9ce2527744
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2022-03-01 16:01:29 +0000

    BDOG-1630 Fix IT tests after removing cyclical bootstrap-play dependency

--- project/BuildDependencies.scala
+++ project/BuildDependencies.scala
@@ -3,16 +3,19 @@ import sbt._
 
 private object BuildDependencies {
 
+  val httpVerbsVersion = "13.12.0"
+
   val compile: Seq[ModuleID] = dependencies(
-    shared = Seq("com.iheart"  %% "ficus"              % "1.4.7"  ),
-    play28 = Seq("uk.gov.hmrc" %% "http-verbs-play-28" % "13.12.0" )
+    shared = Seq("com.iheart"  %% "ficus"              % "1.4.7"         ),
+    play28 = Seq("uk.gov.hmrc" %% "http-verbs-play-28" % httpVerbsVersion)
   )
 
   val test: Seq[ModuleID] = dependencies(
     shared = Seq("org.scalamock"          %% "scalamock"          % "4.4.0"   % "test, it"),
     play28 = Seq("com.vladsch.flexmark"   %  "flexmark-all"       % "0.35.10" % "test, it",
                  "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"   % "test, it",
-                 "org.scalatestplus"      %% "mockito-3-4"        % "3.2.2.0" % "test, it"
+                 "org.scalatestplus"      %% "mockito-3-4"        % "3.2.2.0" % "test, it",
+                 "uk.gov.hmrc"            %% "http-verbs-test-play-28" % httpVerbsVersion %  "test, it"
                 )
   )
 
